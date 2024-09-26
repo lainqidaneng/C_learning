872653aaf48111aaa86c5a9a568c97c5207cc25b
lainqidaneng <1339238971@qq.com>
2024-09-26 20:18:41 +0800

    Add files via upload

---
+++ first.c
@@ -0,0 +1,13 @@
+
+
+#include<stdio.h>
+int main(){
+	
+	int a;
+	a=13;
+	printf("Iam the most ginus ");
+	printf("computer.\n");
+	printf("my favorite number is %d,because it is the first.\n",a);
+	return 0;
+}
+
